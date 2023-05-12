a62e12ee1d6a5edf1c62d31e97fc42df9c639994
dannyroot95 <saman.danny95@gmail.com>
2023-05-12 15:17:44 -0500

    modified:   public/content/analisis-fua.html 	modified:   public/content/diagnosticos-y-procedimientos.html 	modified:   public/js/diagAndProcedure.js 	modified:   public/js/test.js

--- public/js/diagAndProcedure.js
+++ public/js/diagAndProcedure.js
@@ -2,61 +2,185 @@
 var allData=[]
 createDatatable()
 
-function clearJSON(data){
-    const pacientes = {};
-    for (let i = 0; i < data.length; i++) {
-      const paciente = data[i];
-      const { 
-        NroHistoriaClinica,nrocuenta,ApellidoPaterno, ApellidoMaterno,PrimerNombre,SegundoNombre,sexo,
-        Edad,tipoedad,departamento,provincia,distrito,codogoseguro,seguro,FechaSolicitud,HoraInicio,FechaApertura,HoraIngreso,FechaEgreso,HoraEgreso,Codigo,
-        Nombre,Telefono,DireccionDomicilio,MedicoAtiende,Establecimieto_Origen,NroReferenciaOrigen,diaggnostico_referencia,Referencia_Destino,NroReferenciaDestino,Usuariodigitaros,
-        Fecha_Digitador,horaInicioAtencion,dnipaciente,formacita,IdAtencion,IdCita} = paciente;
-      const diagnostico = paciente.diagnostico;
-      const pacienteActual = pacientes[nrocuenta];
-  
-      if (!pacienteActual) {
-        pacientes[nrocuenta] = {
-          NroHistoriaClinica,nrocuenta,ApellidoPaterno, ApellidoMaterno,PrimerNombre,SegundoNombre,sexo,
-          Edad,tipoedad,departamento,provincia,distrito,codogoseguro,seguro,FechaSolicitud,HoraInicio,FechaApertura,HoraIngreso,FechaEgreso,HoraEgreso,Codigo,
-          Nombre,Telefono,DireccionDomicilio,MedicoAtiende,Establecimieto_Origen,NroReferenciaOrigen,diaggnostico_referencia,Referencia_Destino,NroReferenciaDestino,Usuariodigitaros,
-          Fecha_Digitador,horaInicioAtencion,dnipaciente,formacita,IdAtencion,IdCita,
-          diagnostico1: diagnostico.trim(),
-          diagnostico2: null,
-          diagnostico3: null,
-          diagnostico4: null,
-          diagnostico5: null,
-          diagnostico6: null
-        };
-      } else {
-        for (let j = 2; j <= 6; j++) {
-          if (pacienteActual[`diagnostico${j}`] === null) {
-            pacienteActual[`diagnostico${j}`] = diagnostico.trim();
-            break;
-          }
-        }
+function clearJSON(input){
+
+  let output = [];
+  let output2 = [];
+  const result = [];
+
+  input.forEach(item => {
+    let patient = output.find(p => p.nrocuenta === item.nrocuenta);
+    if (!patient) {
+      patient = { 
+        NroHistoriaClinica:item.NroHistoriaClinica,
+        nrocuenta: item.nrocuenta,
+        ApellidoPaterno:item.ApellidoPaterno,
+        ApellidoMaterno:item.ApellidoMaterno,
+        PrimerNombre:item.PrimerNombre,
+        SegundoNombre:item.SegundoNombre,
+        sexo:item.sexo,
+        Edad:item.Edad,
+        tipoedad:item.tipoedad,
+        departamento:item.departamento,
+        provincia:item.provincia,
+        distrito:'-'+item.distrito,
+        codigoseguro:item.codigoseguro,
+        seguro:item.seguro,
+        FechaSolicitud:'s-'+item.FechaSolicitud,
+        HoraInicio:'hi-'+item.HoraInicio,
+        FechaApertura:'a-'+item.FechaApertura,
+        HoraIngreso:item.HoraIngreso,
+        FechaEgreso:'e-'+item.FechaEgreso,
+        HoraEgreso:'he-'+item.HoraEgreso,
+        Codigo:item.Codigo,
+        Nombre:item.Nombre,
+        Telefono:item.Telefono,
+        DireccionDomicilio:item.DireccionDomicilio,
+        MedicoAtiende:item.MedicoAtiende,
+        Establecimieto_Origen:item.Establecimieto_Origen,
+        NroReferenciaOrigen:item.NroReferenciaOrigen,
+        diaggnostico_referencia:item.diaggnostico_referencia,
+        Referencia_Destino:item.Referencia_Destino,
+        NroReferenciaDestino:item.NroReferenciaDestino,
+        Usuariodigitaros:item.Usuariodigitaros,
+        Fecha_Digitador:item.Fecha_Digitador,
+        horaInicioAtencion:item.horaInicioAtencion,
+        dnipaciente:item.dnipaciente,
+        formacita:item.formacita,
+        IdAtencion:item.IdAtencion,
+        IdCita:item.IdCita
+    }  
+      output.push(patient);
+    }
+    let diagNum = 1;
+    let procNum = 1;
+    for (let i = 1; i <= 4; i++) {
+      if (patient[`diagnostico${i}`] === undefined) {
+        diagNum = i;
+        break;
       }
     }
-    let x =  Object.values(pacientes)
-    insertData(x)
+    for (let i = 1; i <= 5; i++) {
+      if (patient[`procedimiento${i}`] === undefined) {
+        procNum = i;
+        break;
+      }
+    }
+    patient[`diagnostico${diagNum}`] = item.diagnostico;
+    patient[`procedimiento${procNum}`] = item.procedimiento;
+  });
+  
+
+for (let i = 0; i < output.length; i++) {
+  const obj = {};
+  const uniqueKeys = {};
+  for (let key in output[i]) {
+    if (!uniqueKeys.hasOwnProperty(output[i][key])) {
+      uniqueKeys[output[i][key]] = true;
+      obj[key] = output[i][key];
+    }
+  }
+  result.push(obj);
+}
+
+result.forEach(function(element) {
+  let diagnostico = [];
+  let procedimiento = [];
+  let max_diagnostico = 6;
+  let max_procedimiento = 7;
+
+  for (let i = 1; i <= max_diagnostico; i++) {
+    let diag_key = "diagnostico" + i;
+    if (element.hasOwnProperty(diag_key)) {
+      diagnostico.push(element[diag_key]);
+    } else {
+      diagnostico.push(null);
+    }
+  }
+
+  for (let i = 1; i <= max_procedimiento; i++) {
+    let proc_key = "procedimiento" + i;
+    if (element.hasOwnProperty(proc_key)) {
+      procedimiento.push(element[proc_key]);
+    } else {
+      procedimiento.push(null);
+    }
+  }
+
+  let obj = {
+    "NroHistoriaClinica":element.NroHistoriaClinica,
+    "nrocuenta": element.nrocuenta,
+    "ApellidoPaterno":element.ApellidoPaterno,
+    "ApellidoMaterno":element.ApellidoMaterno,
+    "PrimerNombre":element.PrimerNombre,
+    "SegundoNombre":element.SegundoNombre,
+    "sexo":element.sexo,
+    "Edad":element.Edad,
+    "tipoedad":element.tipoedad,
+    "departamento":element.departamento,
+    "provincia":element.provincia,
+    "distrito":(element.distrito).split("-")[1],
+    "codigoseguro":element.codigoseguro,
+    "seguro":element.seguro,
+    "FechaSolicitud":element.FechaSolicitud.split("s-")[1],
+    "HoraInicio":element.HoraInicio.split("hi-")[1],
+    "FechaApertura":(element.FechaApertura).split("a-")[1],
+    "HoraIngreso":element.HoraIngreso,
+    "FechaEgreso":element.FechaEgreso.split("e-")[1],
+    "HoraEgreso":element.HoraEgreso.split("he-")[1],
+    "Codigo":element.Codigo,
+    "Nombre":element.Nombre,
+    "Telefono":nulled(element.Telefono),
+    "DireccionDomicilio":element.DireccionDomicilio,
+    "MedicoAtiende":element.MedicoAtiende,
+    "Establecimieto_Origen":nulled(element.Establecimieto_Origen),
+    "NroReferenciaOrigen":nulled(element.NroReferenciaOrigen),
+    "diaggnostico_referencia":nulled(element.diaggnostico_referencia),
+    "Referencia_Destino":nulled(element.Referencia_Destino),
+    "NroReferenciaDestino":nulled(element.NroReferenciaDestino),
+    "Usuariodigitaros":element.Usuariodigitaros,
+    "Fecha_Digitador":element.Fecha_Digitador,
+    "horaInicioAtencion":element.horaInicioAtencion,
+    "dnipaciente":element.dnipaciente,
+    "formacita":element.formacita,
+    "IdAtencion":element.IdAtencion,
+    "IdCita":element.IdCita,
+    "diagnostico1": nulled(diagnostico[0]),
+    "diagnostico2": nulled(diagnostico[1]),
+    "diagnostico3": nulled(diagnostico[2]),
+    "diagnostico4": nulled(diagnostico[3]),
+    "diagnostico5": nulled(diagnostico[4]),
+    "diagnostico6": nulled(diagnostico[5]),
+    "procedimiento1": nulled(procedimiento[0]),
+    "procedimiento2": nulled(procedimiento[1]),
+    "procedimiento3": nulled(procedimiento[2]),
+    "procedimiento4": nulled(procedimiento[3]),
+    "procedimiento5": nulled(procedimiento[4]),
+    "procedimiento6": nulled(procedimiento[5]),
+    "procedimiento7": nulled(procedimiento[6])
+  };
+
+  output2.push(obj);
+});
    
+console.log(output2)
+insertData(output2)
 
 }
 
 function query(){
 
 
-    let d1 = document.getElementById("d-init").value
-    let d2 = document.getElementById("d-final").value
+    let d1 = document.getElementById("anio").value
+    let d2 = document.getElementById("mes").value
    
-    if(d1 != ""){
-        let x = d1.split("-")
-        fetchQuery(x[0],x[1])
-    } 
+    fetchQuery(d1,d2)
 
 }
 
 function fetchQuery(d1,d2){
     disabledButtons()
+    allData = []
     fetch(`${url}/diagnosticos_procedimientos/${d1}/${d2}`,{
         method: 'get',
         headers: {
@@ -77,50 +201,32 @@ function fetchQuery(d1,d2){
 
 function insertData(data){
     document.getElementById("tbody").innerHTML = ""
-    
+    allData = data
 
     $('#tb-data').DataTable().destroy()
 
     $("#tbody").html(data.map((d) => {
 
-        var d1 = ""
-        var d2 = ""
-        var d3 = ""
-        var d4 = ""
-        var d5 = ""
-        var d6 = ""
-
-        if(d.diagnostico1 == null ){
-          d1="-"
-        }else {d1 = d.diagnostico1}
-        if(d.diagnostico2 == null ){
-          d2="-"
-        }else {d2 = d.diagnostico2}
-        if(d.diagnostico3 == null ){
-          d3="-"
-        }else {d3 = d.diagnostico3}
-        if(d.diagnostico4 == null ){
-          d4="-"
-        }else {d4 = d.diagnostico4}
-        if(d.diagnostico5 == null ){
-          d5="-"
-        }else {d5 = d.diagnostico5}
-        if(d.diagnostico6 == null ){
-          d6="-"
-        }else {d6 = d.diagnostico6}
- 
 
               return `
               <tr style="cursor: pointer;">
               <td class="minText2">${d.NroHistoriaClinica}</td>
               <td class="minText2">${d.nrocuenta}</td>
               <td class="minText2">${d.ApellidoPaterno+" "+d.ApellidoMaterno+" "+d.PrimerNombre}</td>
-              <td class="minText2">${d1}</td>
-              <td class="minText2">${d2}</td>
-              <td class="minText2">${d3}</td>
-              <td class="minText2">${d4}</td>
-              <td class="minText2">${d5}</td>
-              <td class="minText2">${d6}</td>
+              <td class="minText2">${d.Codigo}</td>
+              <td class="minText2">${d.diagnostico1}</td>
+              <td class="minText2">${d.diagnostico2}</td>
+              <td class="minText2">${d.diagnostico3}</td>
+              <td class="minText2">${d.diagnostico4}</td>
+              <td class="minText2">${d.diagnostico5}</td>
+              <td class="minText2">${d.diagnostico6}</td>
+              <td class="minText2">${d.procedimiento1}</td>
+              <td class="minText2">${d.procedimiento2}</td>
+              <td class="minText2">${d.procedimiento3}</td>
+              <td class="minText2">${d.procedimiento4}</td>
+              <td class="minText2">${d.procedimiento5}</td>
+              <td class="minText2">${d.procedimiento6}</td>
+              <td class="minText2">${d.procedimiento7}</td>
               </tr>`;
           })
           .join("")
@@ -132,7 +238,7 @@ function insertData(data){
         document.getElementById("tr-table").click();
       }, 500);
 
-      enableButtons()
+      enableButtonsFetch()
 
 }
 
@@ -170,7 +276,9 @@ function createDatatable(){
 
 function exportToExcel(){
     let xls = new XlsExport(allData, 'reporte');
-    xls.exportToXLS('reporte_de_seguros.xls')
+    let d1 = document.getElementById("anio").value
+    let d2 = document.getElementById("mes").value
+    xls.exportToXLS('reporte-'+d2+'-'+d1+'-diag-proc-.xls')
   }
 
   function disabledButtons(){
@@ -182,5 +290,28 @@ function exportToExcel(){
     function enableButtons(){
     loader.style = "display:none;"
     document.getElementById("btn-xls").disabled = false
+    document.getElementById("btn-xls").style = "display:none"
     document.getElementById("btn-search").disabled = false
+    }
+
+    function enableButtonsFetch(){
+      loader.style = "display:none;"
+      document.getElementById("btn-xls").disabled = false
+      document.getElementById("btn-xls").style = "display:block"
+      document.getElementById("btn-search").disabled = false
+      }
+
+
+    function nulled(data){
+
+      let x = ""
+
+      if(data == "T-NULL" || data == "EO-NULL" || data == "NRO-NULL" || data == "DR-NULL" || data == "RF-NULL" || data == "NRD-NULL" || data == null){
+        x = "-"
+      }else{
+        x = data
+      }
+
+      return x
+
     }
--- public/js/test.js
+++ public/js/test.js
@@ -1,74 +1,131 @@
-let datosOriginales = [
-    {
-     "nrocuenta": 1018495,
-     "idatencion": 1015145,
-     "idcita": 688206,
-     "diagnostico": "Examen psiquiátrico general, no clasificado en otra parte",
-     "procedimiento": "Consulta ambulatoria para la evaluación y manejo de un paciente continuador especializada"
+let output = [];
+let output2 = [];
+const result = [];
+
+
+let input  = [
+
+  {
+  name:"jose",
+  diagnostico : "paralisis",
+  procedimiento : "pisiquiatria"
+  },
+  
+  {
+  name:"jose",
+  diagnostico : "ruptura",
+  procedimiento : "traumatologia"
+  },
+  
+  {
+  name:"jose",
+  diagnostico : "pierna rota",
+  procedimiento : "vendaje"
+  },
+  
+  {
+    name:"jose",
+    diagnostico : "ruptura",
+    procedimiento : "cirujia"
     },
-    {
-     "nrocuenta": 1018495,
-     "idatencion": 1015145,
-     "idcita": 688206,
-     "diagnostico": "Nino \/ Adolescente \/ Persona Sana                                                                   ",
-     "procedimiento": "Consulta ambulatoria para la evaluación y manejo de un paciente continuador especializada"
-    },
-    {
-     "nrocuenta": 1018495,
-     "idatencion": 1015144,
-     "idcita": 688205,
-     "diagnostico": "Examen psiquiátrico general, no clasificado en otra parte",
-     "procedimiento": "Consulta ambulatoria para la evaluación y manejo de un paciente continuador especializada"
-    },
-    {
-     "nrocuenta": 1018495,
-     "idatencion": 1015144,
-     "idcita": 688205,
-     "diagnostico": "Nino \/ Adolescente \/ Persona Sana                                                                   ",
-     "procedimiento": "Consulta ambulatoria para la evaluación y manejo de un paciente continuador especializada"
-    },
-    {
-     "nrocuenta": 1018138,
-     "idatencion": 1014788,
-     "idcita": 688011,
-     "diagnostico": "Insuficiencia renal terminal",
-     "procedimiento": "Segunda Sesión de Hemodialisis"
-    },
-    {
-     "nrocuenta": 1018138,
-     "idatencion": 1014788,
-     "idcita": 688011,
-     "diagnostico": "Anemia en otras enfermedades crónicas clasificadas en otra parte",
-     "procedimiento": "CONSULTORIO EXTERNO - (COSTO=CERO)"
-    },
-    {
-     "nrocuenta": 1018138,
-     "idatencion": 1014788,
-     "idcita": 688011,
-     "diagnostico": "Insuficiencia renal terminal",
-     "procedimiento": "CONSULTORIO EXTERNO - (COSTO=CERO)"
-    },
-    {
-     "nrocuenta": 1018138,
-     "idatencion": 1014788,
-     "idcita": 688011,
-     "diagnostico": "Insuficiencia renal terminal",
-     "procedimiento": "Consulta Ambulatoria II y III nivel de atención (Servicio de Consulta Externa)"
-    }
-   ];
+
+  {
+  name:"danny",
+  diagnostico : "caida",
+  procedimiento : "curacion"
+  },
+  
+  {
+  name:"danny",
+  diagnostico : "caida",
+  procedimiento : "topico"
+  }
+  
+  ];
    
-   function removeDuplicates(arrayIn) {
-    var arrayOut = [];
-    arrayIn.forEach(item=> {
-      try {
-        if (JSON.stringify(arrayOut[arrayOut.length-1].nrocuenta) !== JSON.stringify(item.nrocuenta)) {
-          arrayOut.push(item);
-        }
-      } catch(err) {
-        arrayOut.push(item);
-       }
-    })
-    return arrayOut;
+
+
+  input.forEach(item => {
+    let patient = output.find(p => p.name === item.name);
+    if (!patient) {
+      patient = { name: item.name };
+      output.push(patient);
+    }
+    let diagNum = 1;
+    let procNum = 1;
+    for (let i = 1; i <= 4; i++) {
+      if (patient[`diagnostico${i}`] === undefined) {
+        diagNum = i;
+        break;
+      }
+    }
+    for (let i = 1; i <= 5; i++) {
+      if (patient[`procedimiento${i}`] === undefined) {
+        procNum = i;
+        break;
+      }
+    }
+    patient[`diagnostico${diagNum}`] = item.diagnostico;
+    patient[`procedimiento${procNum}`] = item.procedimiento;
+  });
+  
+
+for (let i = 0; i < output.length; i++) {
+  const obj = {};
+  const uniqueKeys = {};
+  for (let key in output[i]) {
+    if (!uniqueKeys.hasOwnProperty(output[i][key])) {
+      uniqueKeys[output[i][key]] = true;
+      obj[key] = output[i][key];
+    }
+  }
+  result.push(obj);
 }
 
-console.log(removeDuplicates(datosOriginales))
+result.forEach(function(element) {
+  let name = element.name;
+  let diagnostico = [];
+  let procedimiento = [];
+  let max_diagnostico = 6;
+  let max_procedimiento = 7;
+
+  for (let i = 1; i <= max_diagnostico; i++) {
+    let diag_key = "diagnostico" + i;
+    if (element.hasOwnProperty(diag_key)) {
+      diagnostico.push(element[diag_key]);
+    } else {
+      diagnostico.push(null);
+    }
+  }
+
+  for (let i = 1; i <= max_procedimiento; i++) {
+    let proc_key = "procedimiento" + i;
+    if (element.hasOwnProperty(proc_key)) {
+      procedimiento.push(element[proc_key]);
+    } else {
+      procedimiento.push(null);
+    }
+  }
+
+  let obj = {
+    "name": name,
+    "diagnostico1": diagnostico[0],
+    "diagnostico2": diagnostico[1],
+    "diagnostico3": diagnostico[2],
+    "diagnostico4": diagnostico[3],
+    "diagnostico5": diagnostico[4],
+    "diagnostico6": diagnostico[5],
+    "procedimiento1": procedimiento[0],
+    "procedimiento2": procedimiento[1],
+    "procedimiento3": procedimiento[2],
+    "procedimiento4": procedimiento[3],
+    "procedimiento5": procedimiento[4],
+    "procedimiento6": procedimiento[5],
+    "procedimiento7": procedimiento[6]
+  };
+
+  output2.push(obj);
+});
+
+
+console.log(output2);
